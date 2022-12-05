2aca4639150ad6772ff97bc1f34eb88905bbca3e
ezDan4ik <danka0804@gmail.com>
2022-12-05 14:39:08 +0200

    first commit

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {  
+        public static string NumberFormatInfo { get; private set; }
+
+        static void Main(string[] args)
+        {
+            string str = "1";
+            int a;
+            bool result = int.TryParse(str, out a);
+            if (result)
+            {
+                Console.WriteLine("Операция успешна");
+            }
+            else
+            {
+                Console.WriteLine("Не удалось (");
+            }
+        }
+    }
+}
